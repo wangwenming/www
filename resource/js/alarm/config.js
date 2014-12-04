99d6710275e197d0421cedb86413dd51b5c9809d
wangwenming <wangwenming1985@gmail.com>
2014-12-04 16:20:48 +0800

    Add config

---
+++ resource/js/alarm/config.js
@@ -0,0 +1,14 @@
+define([
+    'underscore',
+    'zepto'
+], function(_, $) {
+    var HOST = 'http://10.16.29.96',
+        USRE_ID = 'zpw';
+    var config = {
+        url: function(path, params) {
+            return HOST + path + '?' + $.param(_.extend({userId: USRE_ID}, params));
+        }
+    };
+
+    return config;
+});
