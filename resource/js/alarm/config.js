8d5a2566be4c93262d02f1ca72ea672e2668946c
wangwenming <wangwenming1985@gmail.com>
2014-12-04 16:46:28 +0800

    Add setWid js api

--- resource/js/alarm/config.js
+++ resource/js/alarm/config.js
@@ -3,7 +3,7 @@ define([
     'zepto'
 ], function(_, $) {
     var HOST = 'http://10.16.29.96',
-        USRE_ID = 'zpw';
+        USRE_ID = window.wid;
     var config = {
         url: function(path, params) {
             return HOST + path + '?' + $.param(_.extend({userId: USRE_ID}, params));
