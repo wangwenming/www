2cd1a3e5b6d2168940bdb5de8205c44a500aba59
wangwenming <wangwenming1985@gmail.com>
2014-12-05 19:01:57 +0800

    Novel detail

--- resource/js/alarm/views/poster.js
+++ resource/js/alarm/views/poster.js
@@ -6,16 +6,20 @@ define([
     'alarm/collections/event',
     'alarm/views/detail-page'
 ], function(_, $, deferred, Backbone, EventCollection, DetailPageView) {
+    var tpl = _.template($('#tpl-poster').html());
+
     var PosterView = Backbone.View.extend({
         el: $('#page-detail .poster'),
         initialize: function() {
             this.render();
         },
         render: function() {
-            var model = this.model;
+            this.$el.html(tpl(this.model.attributes));
+            /*
             $('img', this.$el).attr('src', model.get('coverImg'));
             $('h3', this.$el).text(model.get('name'));
             $('p', this.$el).text(model.get('title'));
+            */
         }
     });
 
